e171bc01d409f812360a9a0b109513d63194b26d
Jordan Kasper <jordan@jordankasper.com>
2016-10-16 17:55:20 -0400

    Changed up calculator slightly to match new format of assignment

--- calc.js
+++ calc.js
@@ -2,22 +2,17 @@
 // TODO: DEFINE ANY VARIABLES HERE
 
 
-
-// TODO: DEFINE YOUR FUNCTIONS HERE
-
-
-
 /**
  * 		EDIT ME!
  *
  * This function is called each time a button is clicked. You must decide what
  * to do in each case (most likely call another function).
  *
- * @param  {String} buttonValue   The value of the button that was clicked on, for example "6" or "+"
+ * @param  {Number|String} buttonValue   The value of the button that was clicked on, for example 6 or "+"
  */
 function handleButtonClick(buttonValue) {
 
-    // TODO: WRITE SOME OF YOUR CODE HERE
+    // TODO: WRITE YOUR CODE HERE
 
 }
 
@@ -38,73 +33,44 @@ document.querySelector('.run-tests').addEventListener('click', function() {
 
     try {
 
-        // Addition
-        console.assert(add(10, 3) === 13, 'adding 10 and 3 did not result in 13');
-        console.assert(add('10', 3) === 13, 'adding "10" and 3 did not result in 13');
-        console.assert(add('0', 13) === 13, 'adding "0" and 13 did not result in 13');
-        console.assert(add(0, 13) === 13, 'adding 0 and 13 did not result in 13');
-        console.assert(add('-20', 10) === -10, 'adding "-20" and 10 did not result in -10');
-
-        // Subtraction
-        console.assert(subtract(10, 30) === 20, 'subtracting 10 from 30 did not result in 20');
-        console.assert(subtract('10', 30) === 20, 'subtracting "10" from 30 did not result in 20');
-        console.assert(subtract('0', 13) === 13, 'subtracting "0" from 13 did not result in 13');
-        console.assert(subtract(0, 13) === 13, 'subtracting 0 from 13 did not result in 13');
-        console.assert(subtract('-20', 10) === 30, 'subtracting "-20" from 10 did not result in 30');
-
-        // Division
-        console.assert(divide(30, 10) === 3, 'dividing 30 by 10 did not result in 3');
-        console.assert(divide(30, '10') === 3, 'dividing 30 by "10" did not result in 3');
-        console.assert(divide(13, '1') === 13, 'dividing 13 by "1" did not result in 13');
-        console.assert(divide(13, 1) === 13, 'dividing 13 by 1 did not result in 13');
-        console.assert(divide(0, 13) === 0, 'dividing 0 by 13 did not result in 0');
-        console.assert(divide('-2', 10) === -0.2, 'dividing "-2" by 10 did not result in -0.2');
-
-        // Multiplication
-        console.assert(multiply(10, 30) === 300, 'multiplying 10 by 30 did not result in 300');
-        console.assert(multiply('10', 30) === 300, 'multiplying "10" by 30 did not result in 300');
-        console.assert(multiply('0', 13) === 0, 'multiplying "0" by 13 did not result in 0');
-        console.assert(multiply(0, 13) === 0, 'multiplying 0 by 13 did not result in 0');
-        console.assert(multiply('-2', 10) === -20, 'multiplying "-2" by 10 did not result in -20');
-
-        // Button click handler
+        // Button click handler (addition)
         handleButtonClick('1');
         console.assert(testOutput.innerHTML === '1', testOutput.innerHTML, 'pressing `1` did not result in "1" in the display');
         handleButtonClick('3');
-        console.assert(testOutput.innerHTML === '13', testOutput.innerHTML, 'pressing `3` did not result in "13" in the display');
+        console.assert(testOutput.innerHTML === '13', testOutput.innerHTML, 'pressing `3` did not result in correct display (expected "13")');
         handleButtonClick('+');
-        console.assert(testOutput.innerHTML === '13', testOutput.innerHTML, 'pressing `+` did not result in "13" in the display');
+        console.assert(testOutput.innerHTML === '13+', testOutput.innerHTML, 'pressing `+` did not result in correct display (expected "13+")');
         handleButtonClick('7');
-        console.assert(testOutput.innerHTML === '7', testOutput.innerHTML, 'pressing `7` did not result in "7" in the display');
-        handleButtonClick('+');
-        console.assert(testOutput.innerHTML === '20', testOutput.innerHTML, 'pressing `+` did not result in "20" in the display');
-        handleButtonClick('2');
-        console.assert(testOutput.innerHTML === '2', testOutput.innerHTML, 'pressing `2` did not result in "2" in the display');
-        handleButtonClick('.');
-        console.assert(testOutput.innerHTML === '2.', testOutput.innerHTML, 'pressing `.` did not result in "2." in the display');
-        handleButtonClick('7');
-        console.assert(testOutput.innerHTML === '2.7', testOutput.innerHTML, 'pressing `7` did not result in "2.7" in the display');
+        console.assert(testOutput.innerHTML === '13+7', testOutput.innerHTML, 'pressing `7` did not result in correct display (expected "13+7")');
         handleButtonClick('=');
-        console.assert(testOutput.innerHTML === '22.7', testOutput.innerHTML, 'pressing `=` did not result in "22.7" in the display');
-        handleButtonClick('+');
-        console.assert(testOutput.innerHTML === '22.7', testOutput.innerHTML, 'pressing `+` did not result in "22.7" in the display');
-        handleButtonClick('5');
-        console.assert(testOutput.innerHTML === '5', testOutput.innerHTML, 'pressing `5` did not result in "5" in the display');
-        handleButtonClick('=');
-        console.assert(testOutput.innerHTML === '27.7', testOutput.innerHTML, 'pressing `=` did not result in "22.7" in the display');
+        console.assert(testOutput.innerHTML === '20', testOutput.innerHTML, 'pressing `=` did not result in correct display (expected "20")');
 
         // Clear
-        handleButtonClick('clear');
+        handleButtonClick('c');
         console.assert(testOutput.innerHTML === '', testOutput.innerHTML, 'pressing `C` did not clear the display');
 
-        // Second calculation
+        // Subtraction
         handleButtonClick('7');
-        handleButtonClick('+');
+        handleButtonClick('-');
         handleButtonClick('2');
         handleButtonClick('=');
-        console.assert(testOutput.innerHTML === '9', testOutput.innerHTML, 'pressing `7+2=` after clearing did not result in "9"');
+        console.assert(testOutput.innerHTML === '5', testOutput.innerHTML, '`7-2=` did not result in 5');
 
-        console.log('All tests have run. (If you see no errors, they all passed!)');
+        // Division
+        handleButtonClick('7');
+        handleButtonClick('/');
+        handleButtonClick('2');
+        handleButtonClick('=');
+        console.assert(testOutput.innerHTML === '3.5', testOutput.innerHTML, '`7/2=` did not result in 3.5');
+
+        // Multiplication
+        handleButtonClick('7');
+        handleButtonClick('*');
+        handleButtonClick('2');
+        handleButtonClick('=');
+        console.assert(testOutput.innerHTML === '14', testOutput.innerHTML, '`7*2=` did not result in 14');
+
+        console.info('All tests have run. (If you see no errors, they all passed!)');
         updateDisplay('');
 
     } catch(e) {
@@ -122,7 +88,11 @@ document.querySelector('.run-tests').addEventListener('click', function() {
  */
 [].slice.call(document.querySelectorAll('button')).forEach(function(element) {
     element.addEventListener('click', function() {
-        handleButtonClick(this.value);
+        var val = this.value;
+        if (Number(this.value) || this.value === '0') {
+            val = Number(this.value);
+        }
+        handleButtonClick(val);
     });
 });
 
